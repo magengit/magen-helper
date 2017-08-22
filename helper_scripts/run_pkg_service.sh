d18f191857a997a80badd2d212787f6aaed6826f
usetheapi <lifar_tut_net@mail.ru>
2017-08-22 16:54:40 +0300

    [fix] mongo is not required for hwa and bwa

--- helper_scripts/run_pkg_service.sh
+++ helper_scripts/run_pkg_service.sh
@@ -7,6 +7,14 @@ if [ ! -z `$2` ]; then
     log_level=$2
 fi
 
-start_local_mongo.sh || exit 1
+prefix=`echo ${server_script} | cut -d _ -f 1`
+
+if [ $prefix == "hwa" ] || [ $prefix == "bwa" ]; then
+    # mongo not required
+    echo "Mongo is not required, move on.."
+else
+    echo "Starting mongo locally for ${server_script}"
+    bash start_local_mongo.sh || exit 1
+fi
 
 ${server_script} --data-dir ~/data --console-log-level ${log_level}
