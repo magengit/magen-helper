f581540780c4a4d441d70cd97fda6d0f989e591d
Reinaldo Penno <rapenno@gmail.com>
2017-08-30 19:31:24 -0700

    Uploading with twine
    Work around for multi-level build uploads.

--- helper_scripts/artifactory_upload_os.sh
+++ helper_scripts/artifactory_upload_os.sh
@@ -11,9 +11,10 @@ DOCKER_SRC_TAG=$1 # docker image + tag, ex.: magen_ks:v1.3
 DOCKER_IMAGE=$2 # docker image name, ex.: magen-ks
 
 TO_BUILD=${TO_BUILD:-Unset}
-# if [ "$TO_BUILD" = "NATIVE" ]; then
+if [ "$TO_BUILD" = "NATIVE" ]; then
+    twine upload --username "$PYPI_USERNAME" --password  "$PYPI_PASSWORD" dist/*
 #     python3 setup.py bdist_wheel upload -r jfrog || exit 1;
-if [ "$TO_BUILD" = "DOCKER" ]; then
+elif [ "$TO_BUILD" = "DOCKER" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" || exit 1
     docker tag ${DOCKER_SRC_TAG} magendocker/${DOCKER_IMAGE}:latest
     docker push magendocker/${DOCKER_IMAGE}:latest || exit 1
